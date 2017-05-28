f8bd353542abe601493a0f8297f7c142158c64e7
NickYuu <s3310304067@gms.nutc.edu.tw>
2017-05-28 23:21:33 +0800

    樂透

--- 06-樂透/Program.cs
+++ 06-樂透/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,33 @@ namespace _06_樂透
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                var totalArr = new int[42];
+                var resultArr = new ArrayList();
+                var r = new Random();
+
+                for (int i = 0; i < 42; i++)
+                {
+                    totalArr[i] = i + 1;
+                }
+
+                while (resultArr.Count < 6)
+                {
+                    var ran = r.Next(0, 42);
+                    if (!resultArr.Contains(totalArr[ran]))
+                    {
+                        resultArr.Add(totalArr[ran]);
+                    }
+                }
+                resultArr.Sort();
+                for (int i = 0; i < resultArr.Count; i++)
+                {
+                    Console.WriteLine(resultArr[i]);
+                }
+                Console.WriteLine();
+                Console.ReadKey();
+            }
         }
     }
 }
